eb792c3c2cba084b8b5d9d47b6d86524388accb1
hugosenari <hugosenari@gmail.com>
2022-03-12 00:46:41 -0300

    chore: re index binance

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''A Nim library to access the Binance API.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-binance-master.flake = false;
+  inputs.src-binance-master.owner = "Imperator26";
+  inputs.src-binance-master.ref   = "refs/heads/master";
+  inputs.src-binance-master.repo  = "binance";
+  inputs.src-binance-master.type  = "github";
+  
+  inputs."nimsha2".dir   = "nimpkgs/n/nimsha2";
+  inputs."nimsha2".owner = "riinr";
+  inputs."nimsha2".ref   = "flake-pinning";
+  inputs."nimsha2".repo  = "flake-nimble";
+  inputs."nimsha2".type  = "github";
+  inputs."nimsha2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimsha2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-binance-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-binance-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
